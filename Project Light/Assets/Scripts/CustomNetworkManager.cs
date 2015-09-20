769ce1515110964282eefdaad7d5d5ab65ded821
Jacky Chou <jacky.kschou@gmail.com>
2015-09-21 01:45:43 +0800

    imported initial project

---
+++ Project Light/Assets/Scripts/CustomNetworkManager.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+namespace Assets.Scripts
+{
+    public class CustomNetworkManager : NetworkManager
+    {
+        public static CustomNetworkManager Instance;
+
+        public bool IsServer;
+        public GameObject Guider;
+        public GameObject GuiderConroller;
+        public GameObject GuiderView;
+        public GameObject Traveller;
+
+        void Awake()
+        {
+            Instance = FindObjectOfType<CustomNetworkManager>();
+        }
+
+        public override void OnStartServer()
+        {
+            Instantiate(Traveller);
+            IsServer = true;
+        }
+
+        public override void OnClientConnect(NetworkConnection conn)
+        {
+            if (!IsServer)
+            {
+                var guiderConroller = Instantiate(GuiderConroller);
+                var guiderView = Instantiate(GuiderView);
+            }
+        }
+
+        public override void OnServerConnect(NetworkConnection conn)
+        {
+            Debug.Log("conn.address  " + conn.address);
+            Debug.Log("networkAddress  " + networkAddress);
+            if (conn.address != "localServer")
+            {
+                var guilder = Instantiate(Guider);
+            }
+        }
+
+        public override void OnServerAddPlayer(NetworkConnection conn, short playerControllerId)
+        {
+        }
+    }
+}
---
+++ Project Light/Assets/Scripts/NetworkingUI.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+namespace Assets.Scripts
+{
+    public class NetworkingUI : MonoBehaviour
+    {
+        public InputField IpAddressInput;
+
+        public void SetIpAddress(string address)
+        {
+            CustomNetworkManager.Instance.networkAddress = address;
+        }
+
+        void SetPort()
+        {
+            CustomNetworkManager.Instance.networkPort = 7777;
+        }
+
+        public void StartAsClient()
+        {
+            SetIpAddress(IpAddressInput.text);
+            SetPort();
+            CustomNetworkManager.Instance.StartClient();
+        }
+
+        public void StartAsHost()
+        {
+            SetIpAddress(IpAddressInput.text);
+            SetPort();
+            CustomNetworkManager.Instance.StartHost();
+        }
+    }
+}
